2e1c42a4dbbed5ae5b1a49f48c7cd5ecfe6830f1
Fang Lin <fanglin@uw.edu>
2018-08-24 21:30:57 -0700

    remove python 3.4, 3.5

--- setup.py
+++ setup.py
@@ -36,6 +36,5 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
     ],
 )
