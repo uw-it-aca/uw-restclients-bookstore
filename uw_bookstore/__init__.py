50eb9283f34ea36841690852954296d9caccfb84
Fang Lin <fanglin@uw.edu>
2025-07-28 13:57:52 -0700

    New version of textbook API 202507 (#38)
    Implement the 202507 version of textbook API

--- uw_bookstore/__init__.py
+++ uw_bookstore/__init__.py
@@ -1,145 +1,100 @@
 # Copyright 2025 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-"""
-This is the interface for interacting with the UW Bookstore's book service.
-"""
 
 import logging
 from uw_bookstore.dao import Bookstore_DAO
 from restclients_core.exceptions import DataFailureException
-from uw_bookstore.models import Book, BookAuthor
-from concurrent.futures import ThreadPoolExecutor
+from uw_bookstore.models import Book, BookAuthor, Textbook
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import json
 
 logger = logging.getLogger(__name__)
-BOOK_PREFIX = "http://uw-seattle.verbacompare.com/m?section_id="
-API_ENDPOINT = "/uw/json_utf8_202007.ubs"
+API_ENDPOINT = "/uw/json_utf8_202507.ubs"
 DAO = Bookstore_DAO()
 
 
 class Bookstore(object):
     """
-    Get book information for courses.
+    Get textbook requirements for courses.
     """
 
     def get_books_by_quarter_sln(self, quarter, sln):
-        url = "{}?quarter={}&{}&returnlink=t".format(
-            API_ENDPOINT,
-            quarter,
-            self._get_sln_string(sln))
+        """
+        quarter: str, sln: str -> Union[Textbook, Exception]
+        """
+        url = f"{API_ENDPOINT}?quarter={quarter}&sln1={sln}&returnlink=t"
+        logger.debug(f"get_books {url}")
+        try:
+            data = self.get_url(url)
+        except Exception as ex:
+            logger.error(f"{url}  {ex}")
+            return ex
+            # Pass up the error of individual sln book fetching
+
+        books = []
+        value = data.get(str(sln), [])
+        if value and isinstance(value, list):
+            for book_data in value:
+                book = Book(
+                    isbn=book_data.get("isbn"),
+                    title=book_data.get("title"),
+                    price=book_data.get("price"),
+                    lowest_price=book_data.get("lowest_price"),
+                    highest_price=book_data.get("highest_price"),
+                    used_price=book_data.get("used_price"),
+                    is_required=book_data.get("required"),
+                    notes=book_data.get("notes"),
+                    cover_image_url=book_data.get("cover_image"),
+                )
+                book.authors = [
+                    BookAuthor(name=a.get("name"))
+                    for a in book_data.get("authors", [])
+                    if a.get("name")
+                ]
+                books.append(book)
+        link = data.get("ubsLink", {})
+        textbook = Textbook(
+            course_id=link.get("course_id"),
+            search_url=link.get("search")
+        )
+        textbook.books = books
+        logger.debug(f"get_books {url} ==> {str(textbook)}")
+        return textbook
+
+    def get_url(self, url):
         response = DAO.getURL(url, {"Accept": "application/json"})
-        if response:
-            logger.debug("get_books_by_quarter_sln {} =={}==> {}".format(
-                url, response.status, response.data))
+        logger.debug(f"{url} ==> {response.status} ==> {response.data}")
         if response.status != 200:
             raise DataFailureException(url, response.status, response.data)
 
         try:
-            data = json.loads(response.data)
+            return json.loads(response.data)
         except Exception as ex:
+            logger.debug(f"{url} ==> {ex} ==> {response.data}")
             raise DataFailureException(
-                url, response.status,
-                {'exception': ex, 'data': response.data})
+                url, 200, f"InvalidData: {ex} {response.data[:100]}")
 
-        books = []
-
-        sln_data = data[str(sln)]
-
-        if len(sln_data):
-            for book_data in sln_data:
-                book = Book()
-                book.isbn = book_data["isbn"]
-                book.title = book_data["title"]
-                book.price = book_data["price"]
-                book.lowest_price = book_data["lowest_price"]
-                book.highest_price = book_data["highest_price"]
-                book.used_price = book_data["used_price"]
-                book.is_required = book_data["required"]
-                book.notes = book_data["notes"]
-                book.cover_image_url = book_data["cover_image"]
-                book.authors = []
-
-                for author_data in book_data["authors"]:
-                    author = BookAuthor()
-                    author.name = author_data["name"]
-                    book.authors.append(author)
-                logger.debug(str(book))
-                books.append(book)
-        return books
-
-    def get_books_for_schedule(self, schedule):
+    def get_textbooks(self, quarter, sln_set):
         """
-        Returns a dictionary of data.  SLNs are the keys, an array of Book
-        objects are the values.
+        quarter: str, sln_set: Set[str])
+        Returns a Dict[sln str, Union[Textbook, Exception]]
         """
-        books = {}
-        slns = self._get_slns(schedule)
-        if len(slns) == 0:
-            return books
-
-        with ThreadPoolExecutor(max_workers=10) as executor:
-            results = executor.map(
-                    self.get_books_by_quarter_sln,
-                    [schedule.term.quarter] * len(slns),
-                    slns
-                    )
-            executor.shutdown(wait=True)
-
-        for result, sln in zip(results, slns):
-            books[sln] = result
-
-        return books
-
-    def get_url_for_schedule(self, schedule):
-        """
-        Returns a link to verba.  The link varies by campus and schedule.
-        Multiple calls to this with the same schedule may result in
-        different urls.
-        """
-        url = self._get_url(schedule)
-        if url is None:
+        if not quarter or not sln_set:
             return None
-        response = DAO.getURL(url, {"Accept": "application/json"})
-        if response.status != 200:
-            raise DataFailureException(url, response.status, response.data)
+        max_workers = min(len(sln_set), 13)
+        books = {}
+        logger.debug(f"get_textbooks for {quarter} {sln_set}")
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            task_to_sln = {
+                executor.submit(
+                    self.get_books_by_quarter_sln, quarter, sln): sln
+                for sln in sln_set
+            }
 
-        data = json.loads(response.data)
+            for task in as_completed(task_to_sln):
+                sln = task_to_sln[task]
+                books[sln] = task.result()
+                logger.debug(f"Task for {sln} returned {books[sln]}")
 
-        if "ubsLink" in data:
-            return data["ubsLink"][0]["search"]
-
-    def _get_url(self, schedule):
-        sln_string = self._get_slns_string(schedule)
-        if sln_string:
-            url = "{}?quarter={}&{}&returnlink=t".format(
-                API_ENDPOINT,
-                schedule.term.quarter,
-                sln_string
-            )
-            return url
-        return None
-
-    def _get_sln_string(self, sln):
-        return "sln1={}".format(sln)
-
-    def _get_slns(self, schedule):
-        slns = []
-        for section in schedule.sections:
-            if not section.is_campus_tacoma():
-                sln = section.sln
-                if sln and sln not in slns:
-                    slns.append(sln)
-        return slns
-
-    def _get_slns_string(self, schedule):
-        valid_slns = self._get_slns(schedule)
-        if len(valid_slns):
-            slns = []
-            sln_count = 1
-            for sln in valid_slns:
-                slns.append("sln{}={}".format(sln_count, sln))
-                sln_count += 1
-            sln_string = "&".join(slns)
-            return sln_string
-        return None
+        return books
